73db43079ef0461e6861ad18b8347084e788b9fa
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-01-20 20:06:22 +0100

    solution phonebook backend step8

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+const customMorgan = morgan((tokens, req, res) => {
+  const logString = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ]
+
+  if (tokens.method(req, res) === 'POST') {
+    logString.push(JSON.stringify(req.body))
+  }
+
+  return logString.join(' ')
+})
+
+app.use(customMorgan)
 
 let persons = [
   {
@@ -49,15 +66,15 @@ app.post('/api/persons', (request, response) => {
   const body = request.body
 
   if (!body.name) {
-    return response.status(400).json({error: 'name missing'})
+    return response.status(400).json({ error: 'name missing' })
   } else if (!body.number) {
-    return response.status(400).json({error: 'number missing'})
+    return response.status(400).json({ error: 'number missing' })
   } else if (persons.map(p => p.name).includes(body.name)) {
-    return response.status(400).json({error: 'name must be unique'})
+    return response.status(400).json({ error: 'name must be unique' })
   }
 
   const person = {
-    id: Math.ceil(Math.random()*1000),
+    id: Math.ceil(Math.random() * 1000),
     name: body.name,
     number: body.number
   }
@@ -74,7 +91,7 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(p => p.id !== id)
     response.status(204).end()
   } else {
-    response.status(404).end
+    response.status(404).end()
   }
 })
 
