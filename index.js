7d5d998913558e32c1909e95546e464d06139428
Leo Fuhrmann <leo-fuhrmann@gmx.net>
2023-01-20 18:18:24 +0100

    solution phonebook backend step3

--- index.js
+++ index.js
@@ -4,44 +4,53 @@ const app = express()
 // app.use(express.json())
 
 const persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
 ]
 
-// const persons = []
-
 app.get('/api/persons', (request, response) => {
-    console.log('request on /api/persons registered')
-    
-    persons
+  console.log('request on /api/persons registered')
+
+  persons
     ? response.json(persons)
     : response.status(404).end()
 })
 
-app.get('/info', (request, response) => {
-    console.log('request on /info registered')
+app.get('/api/persons/:id', (request, response) => {
+  console.log(`request on /api/persons/${request.params.id}`)
 
-    const responseHtml = `<p>Phonebook has info for ${persons.length} people</p>
-    ${Date()}`
-    response.send(responseHtml)
+  const person = persons.find(p => p.id === Number(request.params.id))
+
+  person
+  ? response.json(person)
+  : response.status(404).end()
+  
+})
+
+app.get('/info', (request, response) => {
+  console.log('request on /info registered')
+
+  const responseHtml = `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${Date()}</p>`
+  response.send(responseHtml)
 })
 
 const PORT = 3001
